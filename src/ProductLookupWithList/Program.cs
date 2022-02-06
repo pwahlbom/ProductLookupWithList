73acbf6213df0c6dba383db748245c6a1ddcc1ef
Philip Wahlbom <philip@wahlbom.net>
2022-02-06 17:27:56 -0500

    resync

--- src/ProductLookupWithList/Program.cs
+++ src/ProductLookupWithList/Program.cs
@@ -5,10 +5,72 @@ using System.Threading.Tasks;
 
 namespace ProductLookupWithList
 {
+    class DoIt
+    {
+
+    }
+
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    class ProductCatalog
+    {
+        public List<Product> Products { get; set; }
+
+        public ProductCatalog()
+        {
+            Products = new List<Product>();
+
+            Products.Add(new Product { ID = 12345, Name = "Widget", Price = 8.99m });
+            Products.Add(new Product { ID = 12346, Name = "Fidget", Price = 6.99m });
+            Products.Add(new Product { ID = 12347, Name = "Midget", Price = 4.99m });
+        }
+
+        public Product Lookup(int ProductID)
+        {
+            var ProductReturn = new Product();
+            ProductReturn = null;
+
+            foreach (var product in this.Products)
+            {
+                if (product.ID == ProductID)
+                {
+                    ProductReturn = product;
+                }
+            }
+            return ProductReturn;
+        }
+    }
+
     public class Program
     {
+
+
         public static void Main(string[] args)
         {
+            var foundProduct = new Product();
+            var productCatalog = new ProductCatalog();
+            int intLookupID = 0;
+
+            Console.WriteLine("What ProductID should I look for?");
+            var LookUpID = Console.ReadLine();
+            intLookupID = int.Parse(LookUpID);
+            foundProduct = productCatalog.Lookup(intLookupID);
+
+            if (foundProduct != null)
+            {
+                Console.WriteLine(foundProduct.Name);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Couldn't find it");
+                Console.ReadLine();
+            }
         }
     }
 }
